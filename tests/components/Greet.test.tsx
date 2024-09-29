f21342b6c0261289f5d3ef026d4887a6c69d5663
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-29 11:39:51 +0530

    test: added test for the Greet component

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { it, expect, describe } from "vitest";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe('Greet', () => {
+    it('should render Hello with the name when name is provided', () => {
+        render(<Greet name="Hanish" />);
+        const heading = screen.getByRole("heading");
+        expect(heading).toBeInTheDocument();
+        expect(heading).toHaveTextContent(/hanish/i);
+    });
+    it("should render login button when the provided name is empty", () => { 
+        render(<Greet />);
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveTextContent(/login/i);
+    });
+});
