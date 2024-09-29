25d1ba9a580fa7cd1a3f4e687f0f3be2dd798843
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-29 11:49:06 +0530

    refactor: removed testinglibrary/jest-dom/vitest import and formatted file

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,15 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "../../src/components/Greet";
-import "@testing-library/jest-dom/vitest";
 
-describe('Greet', () => {
-    it('should render Hello with the name when name is provided', () => {
+describe("Greet", () => {
+    it("should render Hello with the name when name is provided", () => {
         render(<Greet name="Hanish" />);
         const heading = screen.getByRole("heading");
         expect(heading).toBeInTheDocument();
         expect(heading).toHaveTextContent(/hanish/i);
     });
-    it("should render login button when the provided name is empty", () => { 
+    it("should render login button when the provided name is empty", () => {
         render(<Greet />);
         const button = screen.getByRole("button");
         expect(button).toBeInTheDocument();
